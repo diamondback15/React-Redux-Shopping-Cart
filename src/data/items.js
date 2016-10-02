910775e37490cf8bcb215f1f8fbacf24f202dcdc
Julien Tremblay <info@julientremblay.ca>
2016-10-02 13:58:14 -0400

    Fix thumbnail size

--- src/data/items.js
+++ src/data/items.js
@@ -1,20 +1,20 @@
 export const cake = {
   id: 'cake',
   title: 'Cake',
-  image: 'http://i.imgur.com/Syl84O4.jpg',
+  image: 'http://i.imgur.com/bINgrKy.jpg',
   price: 2.44,
 };
 
 export const waffle = {
   id: 'waffle',
   title: 'Waffle',
-  image: 'http://i.imgur.com/jyS1LHA.png',
+  image: 'http://i.imgur.com/9XAa5YE.jpg',
   price: 5.23,
 };
 
 export const chocolate = {
   id: 'chocolate',
   title: 'Chocolate',
-  image: 'http://i.imgur.com/SX6fy96.jpg',
+  image: 'http://i.imgur.com/5K4IXvK.jpg',
   price: 3.32,
 };
